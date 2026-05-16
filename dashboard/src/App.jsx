2d9d23ff79dbbeaebe8092f7769a203eac37a163
rayxxee <rayyanaiza74@gmail.com>
2026-05-16 16:17:39 +0500

    Fix sidebar overlap issue in dashboard layout

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <main className="flex-1 ml-64 p-8">
+      <main className="flex-1 ml-64 p-8 w-full" style={{ marginLeft: '16rem' }}>
         <Outlet />
       </main>
     </div>
